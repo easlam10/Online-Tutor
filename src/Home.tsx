c772768df9d2f6e5816b08e96c8cc3c2bca52830
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-02-17 02:52:08 -0800

    Navbar created

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './footer'
+
+export default function 
+() {
+  return (
+   <>
+   <Navbar />
+   <Footer />
+   </>
+  )
+}
