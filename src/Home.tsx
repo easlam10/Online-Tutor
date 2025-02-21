6749787a5ddf11003d32088f31fef745fe1f3233
Awais-Arjumand <awaisarjumand557@gmail.com>
2025-02-21 12:30:24 +0500

    Make Become A Turtor Page Full Responsive

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,13 @@
 import { CoursesMain } from "./utils/courses/page";
+import { BecomeATutorMain } from "./utils/requesATutor/page";
 import { SubjectMain } from "./utils/subject/page";
 
 export default function () {
   return (
     <>
     {/* <SubjectMain/> */}
-    <CoursesMain/>
+    {/* <CoursesMain/> */}
+    <BecomeATutorMain/>
       {/* <Navbar /> */}
       {/* <Footer /> */}
     </>
